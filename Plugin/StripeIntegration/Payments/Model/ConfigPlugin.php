3d14f9ef1e1de714afa77efb5818121abbe89778
Aimane Couissi <contact@aimanecouissi.com>
2025-09-11 18:30:04 +0100

    first commit

---
+++ Plugin/StripeIntegration/Payments/Model/ConfigPlugin.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AimaneCouissi\StripePaymentMetadataStoreView\Plugin\StripeIntegration\Payments\Model;
+
+use Magento\Sales\Model\Order;
+use StripeIntegration\Payments\Model\Config;
+
+class ConfigPlugin
+{
+    /**
+     * Adds store view name to Stripe payment metadata.
+     *
+     * @param Config $subject
+     * @param array $result
+     * @param Order $order
+     * @return array
+     */
+    public function afterGetMetadata(Config $subject, array $result, Order $order): array
+    {
+        $result['Store View'] = $order->getStore()->getname();
+        return $result;
+    }
+}
